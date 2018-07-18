a4e118e41eb60b1e97b8dc3a4490ff2100d977d5
wuwengang <15244909057.ww@gmail.com>
2018-07-18 14:10:21 +0800

    Update client.sh

--- client/client.sh
+++ client/client.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 IP=${IP}
 PORT=${PORT}
-./proxy dns -S http -T tls -P "$IP:$PORT" -C proxy.crt -K proxy.key -P :53
+./proxy dns -S http -T tls -P "$IP:$PORT" -C proxy.crt -K proxy.key -p :53
